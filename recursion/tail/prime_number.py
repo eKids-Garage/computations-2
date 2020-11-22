97c6d12712e44e51b415604ca2ba98a4b6f4081e
IvanSuboch <ivan.suboch9@gmail.com>
2020-11-22 20:11:39 +0000

    With while

--- recursion/tail/prime_number.py
+++ recursion/tail/prime_number.py
@@ -13,13 +13,11 @@ def is_prime(n, k = 2):
     return is_prime(n, k + 1)
 
 def is_prime_tail(n, k = 2):
-  if k > n**0.5:
-    return ("YES")
-  elif n % k == 0:
-    return("NO") 
-  k = k + 1
-  return is_prime(n, k)
-
+  while k <= n**0.5:
+    if n % k == 0:
+      return("NO") 
+    k = k + 1
+  return ("YES")
 
 
 
